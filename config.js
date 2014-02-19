aa2cf8b64cd68f492cdc0fd18735806426e0efc7
David Spriggs <spriggs.d@gmail.com>
2014-02-18 20:35:24 -0800

    Added reputation to schema and updated account page.

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.port = process.env.PORT || 3000;
 exports.mongodb = {
   uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'localhost/blister'
 };
-exports.companyName = 'City of Big Bear Lake';
+exports.companyName = 'Big Bear Lake';
 exports.projectName = 'Tripn\' Big Bear';
 exports.systemEmail = '<youraddy>@esri.com';
 exports.cryptoKey = 'k3yb0ardc4tzzz';

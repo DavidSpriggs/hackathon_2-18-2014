6808615eeb0a02dd93aa6bfa2c76a4ab5e4e289d
David Spriggs <spriggs.d@gmail.com>
2014-02-19 09:27:33 -0800

    Added dojo layer and activities/home route and page.

---
+++ views/activities/home/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+exports.init = function(req, res) {
+	res.locals.user.nameParts = req.user.roles.account.name;
+	res.locals.user.reputation = req.user.roles.account.reputation;
+	res.render('activities/home/index');
+};
